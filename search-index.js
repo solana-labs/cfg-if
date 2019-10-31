db7b5ee7b9c33c960a63efc136ecbd69bbfd23fb
ci <ci>
2019-10-31 19:28:48 +0000

    init

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=["cfg_if"];
+
+searchIndex["cfg_if"]={"doc":"A macro for defining `#[cfg]` if-else statements.","i":[[14,R[0],R[0],"The main macro provided by this crate. See crate…",N,N]],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
